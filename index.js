ecd6ade69ac43242751e00f8cb03d90df440cf61
shinieaggarwal72 <shinieaggarwal72@gmail.com>
2025-06-26 11:44:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+app.get('/login', (req, res)=>{
+    res.send('Login details')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
